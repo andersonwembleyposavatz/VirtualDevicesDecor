1c2048b70d8dd0947884d52b0219b57d0777a699
Anderson Wembley <posavatza@gmail.com>
2021-10-01 21:01:30 -0300

    Decor devices tiles

---
+++ devicetypes/andersonwembleyposavatz/virtual-decor-switch-and-contact-sensor.src/virtual-decor-switch-and-contact-sensor.groovy
@@ -0,0 +1,109 @@
+/**
+ *  VIRTUAL DECOR Switch and Contact Sensor
+ *
+ *  Copyright 2021 Anderson W Posavatz
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "VIRTUAL DECOR Switch and Contact Sensor", namespace: "andersonwembleyposavatz", author: "Anderson W Posavatz", vid: "generic-contact", runLocally: true , executeCommandsLocally: true , ocfDeviceType: "x.com.st.d.sensor.contact", 
+ ) {
+		capability "Actuator"
+		capability "Switch"
+		capability "Refresh"
+		capability "Sensor"
+		capability "Contact Sensor"
+		capability "Health Check"
+	}
+
+	simulator {
+		
+	}
+
+	tiles {
+		standardTile("toggle", "device.switch", width: 3, height: 3) {
+			state("off", label:'${name}', action:"on", icon:"st.contact.contact.closed", backgroundColor:"#00A0DC", nextState:"turningOn")
+			state("on", label:'${name}', action:"off", icon:"st.contact.contact.open", backgroundColor:"#cccccc", nextState:"turningOff")
+			state("turningOn", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#cccccc")
+			state("turningOff", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#00A0DC")
+			
+		}
+		standardTile("open", "device.contact", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#00A0DC"
+		}
+		standardTile("closed", "device.contact", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#cccccc"
+			
+        }
+        
+        standardTile("battery", "device.battery", inactiveLabel: false, decoration: "flat") {
+             state "battery", label: "88", unit: "%"
+       
+       }
+
+		main (["contact", "toggle"])
+		details(["toggle", "contact", "on", "off", "open", "closed", "battery"])
+	}
+}
+
+def parse(String description) {
+	log.trace "parse($description)"
+}
+
+def on() {
+	sendEvent(name: "switch", value: "turningOn")
+    runIn(2, finishOpening)
+}
+
+def off() {
+    sendEvent(name: "switch", value: "turningOff")
+	runIn(2, finishClosing)
+}
+
+def open() {
+	sendEvent(name: "switch", value: "turningOn")
+    runIn(2, finishOpening)
+}
+
+def closed() {
+    sendEvent(name: "switch", value: "turningOff")
+	runIn(2, finishClosing)
+}
+
+def finishOpening() {
+    sendEvent(name: "switch", value: "on")
+    sendEvent(name: "contact", value: "open")
+}
+
+def finishClosing() {
+    sendEvent(name: "switch", value: "off")
+    sendEvent(name: "contact", value: "closed")
+}
+
+def installed() {
+	log.trace "Executing 'installed'"
+	initialize()
+}
+
+def updated() {
+	log.trace "Executing 'updated'"
+	initialize()
+}
+
+def initialize() {
+	sendEvent(name: "switch", value: "on")
+    sendEvent(name: "contact", value: "open")
+    sendEvent(name: "battery", value: device.currentValue("battery") ?: 88)
+    
+    sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
+	sendEvent(name: "healthStatus", value: "online")
+	sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
+}
---
+++ devicetypes/smartthings/virtual-decor-generic-lock.src/virtual-decor-generic-lock.groovy
@@ -0,0 +1,97 @@
+/**
+ *  Copyright 2017 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  An enhanced virtual lock that allows for testing failure modes
+ *  Author: SmartThings
+ *  Date: 2017-08-07
+ *
+ */
+metadata {
+	definition (name: "VIRTUAL DECOR Generic Lock", namespace: "smartthings", author: "SmartThings", minHubCoreVersion: '000.017.0012', executeCommandsLocally: true, ) {
+		capability "Actuator"
+        capability "Sensor"
+        capability "Health Check"
+        capability "Switch"
+        capability "Lock"
+        capability "Battery"
+        capability "Refresh"
+
+	}
+
+	simulator {
+		status "locked": "lock:locked"
+		status "unlocked": "lock:unlocked"
+
+		reply "lock": "lock:locked"
+		reply "unlock": "lock:unlocked"
+	}
+
+	tiles {
+		standardTile("lock", "device.lock", width: 2, height: 2) {
+			state "unlocked", label:'unlocked', action:"lock.lock", icon:"st.locks.lock.unlocked", backgroundColor:"#ffffff"
+			state "locked", label:'locked', action:"lock.unlock", icon:"st.locks.lock.locked", backgroundColor:"#00A0DC"
+		}
+		standardTile("lock", "device.lock", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'lock', action:"lock.lock", icon:"st.locks.lock.locked"
+		}
+		standardTile("unlock", "device.lock", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'unlock', action:"lock.unlock", icon:"st.locks.lock.unlocked"
+		}
+
+		main "lock"
+		details(["toggle", "lock", "unlock", "refresh"])
+	}
+}
+
+def parse(String description) {
+	def pair = description.split(":")
+	createEvent(name: pair[0].trim(), value: pair[1].trim())
+}
+
+def lock() {
+	"lock"
+}
+
+def unlock() {
+	"unlock"
+
+}
+
+def off() {
+        sendEvent(name: "lock", value: "unlock")
+
+}
+
+def on() {
+        sendEvent(name: "lock", value: "lock")
+       
+}
+
+def installed() {
+	initialize()
+}
+
+def updated() {
+	initialize()
+}
+
+private initialize() {
+
+    sendEvent(name: "lock", value: "unlock")
+    
+    sendEvent(name: "battery", value: device.currentValue("battery") ?: 74)
+
+	sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
+	sendEvent(name: "healthStatus", value: "online")
+	sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
+    }
+
